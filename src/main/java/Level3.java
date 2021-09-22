8eac1974eb2efa765114f49d4a2ea2c7b9ef9808
brandon_nus <brandoncmy@gmail.com>
2021-09-22 21:54:30 +0800

    tidy-up code

--- src/main/java/Level3.java
+++ src/main/java/Level3.java
@@ -3,27 +3,8 @@ import java.util.Arrays;
 
 public class Level3 {
 
-    public static void printListArray(String[] item){
-        int i = 0;
-
-        while(item[i] != null){
-            System.out.println(i+1 + ". " + item[i]);
-            i++;
-        }
-    }
-
     public static void main(String[] args) {
-
-        String greet = "_________________________________________________\n"
-                + "Hello! I'm Duke\n"
-                + "What can I do for you?\n"
-                + "_________________________________________________\n";
-
-        String exit = "_________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "_________________________________________________\n";
-
-        System.out.println(greet);
+        printGreet();
 
         Scanner in = new Scanner(System.in);
 
@@ -33,11 +14,9 @@ public class Level3 {
         String item = in.nextLine();
         String listStop = "bye";
 
-
-
         while (item != null) {
             if (item.equals(listStop)){
-                System.out.println(exit);
+                printExit();
                 break;
             }else if (item.equals("list")){
                 System.out.println("_________________________________________________\n");
@@ -54,4 +33,35 @@ public class Level3 {
         }
 
     }
+
+    public static void printGreet(){
+
+        String greet
+                = "_________________________________________________\n"
+                + "Hello! I'm Duke\n"
+                + "What can I do for you?\n"
+                + "_________________________________________________\n";
+
+        System.out.println(greet);
+    }
+
+    public static void printExit(){
+
+        String exit = "_________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "_________________________________________________\n";
+
+        System.out.println(exit);
+    }
+
+    public static void printListArray(String[] item){
+        int i = 0;
+
+        while(item[i] != null){
+            System.out.println(i+1 + ". " + item[i]);
+            i++;
+        }
+    }
+
+
 }
