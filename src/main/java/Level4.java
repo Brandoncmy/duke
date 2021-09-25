e825b5ee97dd9f2fd5fc7c7cefd4990399df5d73
brandon_nus <brandoncmy@gmail.com>
2021-09-25 23:04:44 +0800

    update on 25 Sep

--- src/main/java/Level4.java
+++ src/main/java/Level4.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.lang.Integer;
 import java.lang.String;
+import java.lang.*;
 
 
 public class Level4 {
@@ -12,56 +13,93 @@ public class Level4 {
         Task[] listOfTask = new Task [100];
         int taskCount = 0;
 
-        String item = in.nextLine();        //starting item
+        String input = in.nextLine();        //starting item
+        String firstInput = input.split(" ")[0];
         String listStop = "bye";
 
-        while (item != null) {
-            if (item.equalsIgnoreCase(listStop)){
+
+        while (input != null) {
+            if (firstInput.equalsIgnoreCase(listStop)){                                                                      // "exit"
                 printExit();   //exit//
                 break;
-            }else if (item.equals("list")){
+            }else if (firstInput.equalsIgnoreCase("list")){                                                       // print "list"
                 System.out.println("_________________________________________________");
                 printTaskList(listOfTask);
                 System.out.println("_________________________________________________\n");
-            }else if (isInteger(item)){
-                String taskNum = item.split(" ")[1];
+            }else if (firstInput.equalsIgnoreCase("done")){                                                       // make "done"
+                String taskNum = input.split(" ")[1];
                 int num = Integer.parseInt(taskNum) - 1;
 
                 if (listOfTask[num].description !=  null){
-                    listOfTask[num].markAsDone();
-                    String taskD = listOfTask[num].description;
+                    listOfTask[num].markAsDone();                                                                            //list : mark as done
                     System.out.println("_________________________________________________");
                     System.out.println("Nice! I've marked this task as done: \n" + listOfTask[num].getStatusIcon() + listOfTask[num].description);
                     System.out.println("_________________________________________________\n");
-
-
                 }else{
                     System.out.println("There is no task #" + taskNum+1);
                 }
 
             } else {
-                Task t = new Task(item);        //create new task//
-                listOfTask[taskCount] = t;
+                String taskDescription = input.substring(firstInput.length(), input.length());                              // keep task description
+
+                switch (firstInput){
+                    case "todo" :
+                        Todo t = new Todo (taskDescription);
+                        t.type = "todo";
+                        listOfTask[taskCount] = t;
+                        break;
+                    case "deadline" :
+                        Deadline d = new Deadline(taskDescription);
+                        d.type = "deadline";
+                        listOfTask[taskCount] = d;
+                        break;
+                    case "event" :
+                        Event e = new Event(taskDescription);
+                        e.type = "event";
+                        listOfTask[taskCount] = e;
+                        break;
+                }
+
                 System.out.println("_________________________________________________\n"
-                        + "added: " + listOfTask[taskCount].description + "\n"
-                        + "_________________________________________________\n");
+                                    + "Got it. I've added this task: \n"
+                                    + "\t" + listOfTask[taskCount].getType() + listOfTask[taskCount].getStatusIcon() + listOfTask[taskCount].description + "\n"
+                                    + "Now you have " + (taskCount+1) + " tasks in the list." + "\n"
+                                    + "_________________________________________________\n");
+
                 taskCount++;
             }
-            item = in.nextLine();
+            input = in.nextLine();
+            firstInput = input.split(" ")[0];
         }
 
     }
 
-    public static boolean isInteger(String item) {
-        if(item.matches(".*\\d.*")){
-            return true;
-        }else{
-            return false;
+    public static class Todo extends Task {
+
+        public Todo(String description) {
+            super(description);
+        }
+    }
+
+    public static class Deadline extends Task {
+
+        public Deadline(String description) {
+            super(description);
+            this.description = description.replace("/by", "(by:") + ")";
+        }
+    }
+
+    public static class Event extends Task {
+
+        public Event(String description) {
+            super(description);
+            this.description = description.replace("/at", "(at:") + ")";
         }
     }
 
     public static class Task {
         protected String description;
+        protected String type;
         protected boolean isDone;
 
         public Task(String description) {
@@ -80,6 +118,25 @@ public class Level4 {
             this.isDone = true;
             return (this.getStatusIcon()); // mark done task with X
         }
+
+        public String getType(){
+
+            String taskType = this.type;
+
+            switch (taskType){
+                case "todo" :
+                    type = "[T]";
+                    break;
+                case "deadline" :
+                    type = "[D]";
+                    break;
+                case "event" :
+                    type = "[E]";
+                    break;
+            }
+            return type;
+        }
+
     }
     public static void printGreet(){
 
@@ -108,7 +165,7 @@ public class Level4 {
         int i = 0;
 
         while(item[i] != null){
-            System.out.println(i+1 + "." + item[i].getStatusIcon() + item[i].description);
+            System.out.println(i+1 + "." + item[i].getType() + item[i].getStatusIcon() + item[i].description);
             i++;
         }
     }
